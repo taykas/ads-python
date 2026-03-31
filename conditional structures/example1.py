a41c7889ed1cef47a7714efe7317371e63c5f43b
taykas <thaynaschaeffer@gmail.com>
2026-03-31 13:01:37 -0300

    Doing simple conditional structures

---
+++ conditional structures/example1.py
@@ -0,0 +1,22 @@
+# comentario
+'''
+    comentario multilinha
+'''
+
+idade = int(input("Digite sua idade: "))
+
+if(idade >= 18):
+    print("Pode tirar a CNH")
+else:
+    print("Não pode tirar a CNH")
+
+
+minha_idade = 25
+idade_irmao = 21
+
+if minha_idade > idade_irmao:
+    print("eu sou mais velha que meu irmão")
+elif minha_idade == idade_irmao:
+    print("eu e meu irmão temos a mesma idade")
+else:
+    print("meu irmão é mais velho que eu")
