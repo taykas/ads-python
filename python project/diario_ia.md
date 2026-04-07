d0b584e5076d7353b7f4a523436adcb95c2ff4cd
taykas <thaynaschaeffer@gmail.com>
2026-04-06 22:05:58 -0300

    terminando a segunda parte do projeto, iniciando semana 4

---
+++ python project/diario_ia.md
@@ -0,0 +1,14 @@
+# Diário de Bordo de IA - Thayna Kaizi Schaeffer
+
+## 2026-04-06
+### Dificuldade encontrada
+Não tive dificuldades com as propostas dessa etapa do projeto e também não utilizei da inteligência artificial :)
+ 
+### Prompt enviado para a IA
+N/A
+ 
+### Resposta da IA (Resumo)
+N/A
+ 
+### Minha análise e aprendizado
+N/A
--- python project/main.py
+++ python project/main.py
@@ -1,13 +1,30 @@
 comando = input("Digite o comando desejado: ").upper()
 
-while(comando!= "QUIT"):
+while True:
+    if(comando == "QUIT"):
+        break
+
     match comando:
         case "ABOUT":
-            print("===== Bem vindo(a) a Gestão de Albuns da Thayna! <3 =====")
+            print("===== Bem vindo(a) a Gestão de Álbuns da Thayna! <3 =====")
+
+        case "ADD":
+            num = int(input("Quantos álbuns deseja inserir? "))
+
+            if num <= 0:
+                print("Quantidade inválida, tente novamente!")
+            else:
+                print("Digite o nome dos álbuns:")
+                for i in range(num):
+                    nome_album = input(f"\n Álbum número {i}-> ")
+                    print(f"Album: {nome_album}, adicionado com sucesso! :)")
+
         case "QUIT":
             print("Obrigada! saindo do programa...")
+
         case _:
             print("ERRO: Comando inválido, tente novamente!")
 
     comando = input("Digite o comando desejado: ").upper()
+
 print("Até a próxima!")
