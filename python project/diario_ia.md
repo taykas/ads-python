cf3e1b8367d8a31253c491f6c2fd664a74c4a5b6
taykas <thaynaschaeffer@gmail.com>
2026-05-04 15:20:09 -0300

    feat: including json and validations

--- python project/diario_ia.md
+++ python project/diario_ia.md
@@ -1,6 +1,6 @@
 # Diário de Bordo de IA - Thayna Kaizi Schaeffer
 
-## 2026-04-06
+## 2026-04-04
 ### Dificuldade encontrada
 Não tive dificuldades com as propostas dessa etapa do projeto e também não utilizei da inteligência artificial :)
  
--- python project/main.py
+++ python project/main.py
@@ -1,30 +1,105 @@
-comando = input("Digite o comando desejado: ").upper()
+import json
+
+ARQUIVO = "portfolio.json"
+
+# Lê e valida o arquivo
+def carregar_albuns():
+    try:
+        with open(ARQUIVO, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("\n =-=-= ERRO: Arquivo não encontrado. Um novo será criado automaticamente. =-=-=\n")
+        return []
+    except json.JSONDecodeError:
+        print("ERRO: O arquivo está vazio ou corrompido. Os dados serão reiniciados.")
+        return []
+
+# Quando QUIT, salva o arquivo
+def salvar_albuns(lista):
+    with open(ARQUIVO, "w", encoding="utf-8") as f:
+        json.dump(lista, f, indent=4, ensure_ascii=False)
+
+albuns = carregar_albuns()
 
 while True:
-    if(comando == "QUIT"):
-        break
+    comando = input("\nDigite o comando desejado *ABOUT | ADD | QUIT* -> ").strip().upper()
 
+    if  not comando:
+        print("\nERRO: Comando vazio! Tente novamente.")
+        continue
+
+    if comando.isnumeric():
+        print("\nERRO: Comando não pode ser número!")
+        continue
+    break
+
+while True:
     match comando:
         case "ABOUT":
-            print("===== Bem vindo(a) a Gestão de Álbuns da Thayna! <3 =====")
+            print("\n =-=-=-=-= Bem vindo(a) a Gestão de Álbuns da Thayna! <3 =-=-=-=-=\n" \
+            "\nAqui você encontra os álbuns na lista de espera para escutar da Thayna!\n" \
+            "Esse projeto está sendo desenvolvido em Python na matéria de *RACIOCÍNIO COMPUTACIONAL*\n" \
+            "Fique a vontade para utilizar dessa ferramenta para não perder nenhum álbum!")
 
         case "ADD":
-            num = int(input("Quantos álbuns deseja inserir? "))
+            while True:
+                try:
+                    valor = input("\nQuantos álbuns deseja inserir? ").strip()
 
-            if num <= 0:
-                print("Quantidade inválida, tente novamente!")
-            else:
-                print("Digite o nome dos álbuns:")
-                for i in range(num):
-                    nome_album = input(f"\n Álbum número {i}-> ")
-                    print(f"Album: {nome_album}, adicionado com sucesso! :)")
+                    if not valor:
+                        print("\nERRO: Valor vazio! Digite um número.")
+                        continue
+
+                    if not valor.isdigit():
+                        print("\nERRO: Erro de digitação! Digite apenas números.")
+                        continue
+
+                    num = int(valor)
+
+                    if num <= 0:
+                        print("\nERRO: Quantidade inválida! O número deve ser maior que zero.")
+                        continue
+
+                    break
+
+                except ValueError:
+                    print("\nERRO: Erro de conversão! Tente novamente.")
+
+            print(f"\nQuantidade Desejada: {num}, perfeito! \n\n=-=-= Cadastro de Álbuns =-=-=")
+
+            for i in range(num):
+                while True:
+                    nome_album = input(f"\nÁlbum número {i+1}: ").strip()
+
+                    if not nome_album:
+                        print("\n =-=-= ERRO: Nome não pode ser vazio! =-=-=\n")
+                        continue
+
+                    break
+
+                albuns.append(nome_album)
+                print(f"\nÁlbum: {nome_album}, adicionado com sucesso! :)")
 
         case "QUIT":
+            salvar_albuns(albuns)
+            print("\nSUCESSO: Dados salvos com sucesso!")
             print("Obrigada! saindo do programa...")
+            break
 
         case _:
-            print("ERRO: Comando inválido, tente novamente!")
+            print("\nERRO: Comando inválido! Use: ABOUT | ADD | QUIT")
 
-    comando = input("Digite o comando desejado: ").upper()
+    while True:
+        comando = input("\nDigite o comando desejado: ").strip().upper()
 
-print("Até a próxima!")
+        if not comando:
+            print("\nERRO: Comando vazio! Tente novamente.")
+            continue
+
+        if comando.isnumeric():
+            print("\nERRO: Comando não pode ser número!")
+            continue
+
+        break
+
+print("\n =-=-= Até a próxima! =-=-=")
